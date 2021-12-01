4f72e0c645e1faf2452111fa1a1c2f82b64e5669
DoheumPark <pirbak@gmail.com>
2021-12-01 11:17:48 +0900

    first

---
+++ src/main/java/com/koreait/basic/user/UserJoinServlet.java
@@ -0,0 +1,24 @@
+package com.koreait.basic.user;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/user/join")
+public class UserJoinServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        req.setAttribute("title", "회원가입");
+        req.setAttribute("page", "user/join");
+        String jsp = "/WEB-INF/view/layout.jsp";
+        req.getRequestDispatcher(jsp).forward(req, res);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/webapp/WEB-INF/view/user/join.jsp
@@ -0,0 +1,2 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<div>join 페이지입니다.</div>
