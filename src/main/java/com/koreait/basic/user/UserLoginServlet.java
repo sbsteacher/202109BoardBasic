da024ccd21ca2e9f8f15c1d18762d5aa593789f2
DoheumPark <pirbak@gmail.com>
2021-12-01 13:26:04 +0900

    css 적용

--- src/main/java/com/koreait/basic/user/UserLoginServlet.java
+++ src/main/java/com/koreait/basic/user/UserLoginServlet.java
@@ -9,11 +9,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/user/join")
-public class UserJoinServlet extends HttpServlet {
+@WebServlet("/user/login")
+public class UserLoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
-        Utils.displayView("회원가입", "user/join", req, res);
+        Utils.displayView("로그인", "user/login", req, res);
     }
 
     @Override
